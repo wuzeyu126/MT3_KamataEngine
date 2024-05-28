d175c1a917be5040d0d959be1d611b6cb619a1eb
wuzeyu126 <wuzeyu126@gmail.com>
2024-05-28 11:01:38 +0900

    00-01確認課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -11,9 +11,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
+
+	Vector3 v1{ 1.0f,3.0f,-5.0f };
+	Vector3 v2{ 4.0f,-1.0f,2.0f };
+	float k = 4.0f;
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -28,6 +33,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -35,6 +46,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, resultAdd, ":Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, ":Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, ":Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f :Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f :Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, ":Normalize");
 
 		///
 		/// ↑描画処理ここまで
